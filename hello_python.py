49a6f5189ccfc022b29d8039786e9b93c8199109
Jesimish <jsscmccrsk@gmail.com>
2025-06-01 19:13:08 -0700

    commit message

---
+++ hello_python.py
@@ -0,0 +1,12 @@
+name = input("what is your name: ")
+print("Hello, " + name + "! It is wonderful to meet you <3!")
+# prints: "Hello, Jessica." (if the user enters Jessica)
+color = input("what color shirt are you wearing today: ")
+if color == "blue":
+    print("lovely choice! Matches the sky!")
+if color == "brown":
+    print("interesting... like dirt... or chocolate! Love that for you!")
+if color == "green":
+    print("Ooooo like plants!")
+else:
+    print("I bet you look fabulous \(^-^)/ ")
